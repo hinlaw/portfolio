199ec0d618712af861fc9d7421fad9339e3ce167
HinLaw <hinlaw2024@gmail.com>
2025-10-11 02:41:00 +0800

    add category section with scroll effect

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from './components/HeroSection';
 import BenefitsSection from './components/BenefitsSection';
+import CategoriesSection from './components/CategoriesSection';
 import FooterSection from './components/FooterSection';
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <HeroSection />
       <BenefitsSection />
+      <CategoriesSection />
       <FooterSection />
     </div>
   );
