41a1b550e8d2f0ad129445c3b8b29861011c586d
HinLaw <hinlaw2024@gmail.com>
2026-01-24 19:12:13 +0800

    reset

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,8 @@
-import HeroSection from '@/components/hero-section';
-import CategoriesSection from '@/components/categories-section';
-import FooterSection from '@/components/footer-section';
 
 export default function Home() {
   return (
     <div>
-      <HeroSection />
-      <CategoriesSection />
-      <FooterSection />
+      hello
     </div>
   );
 }
